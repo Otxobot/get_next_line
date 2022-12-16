b1da0df7d56a815dba98d46ff047ead8313b03b1
Otxobot <otxoboy64@gmail.com>
2022-12-16 13:10:29 +0100

    getnextline simple

--- get_next_line.c
+++ get_next_line.c
@@ -6,51 +6,55 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 13:33:44 by abasante          #+#    #+#             */
-/*   Updated: 2022/12/13 16:06:35 by abasante         ###   ########.fr       */
+/*   Updated: 2022/12/16 13:05:48 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_line(char *line)
-{
-	int		i;
-	char	*s;
-
-	i = 0;
-	if (!*line)
-		return (NULL);
-	while (line[i] && line[i] != '\n')
-	i++;
-	s = (char *)malloc(sizeof(char) * (i + 2));
-	if (!s)
-		return (NULL);
-	while (line[i] && line[i] != '\n')
-	{
-		s[i] = line[i];
-		i++;
-	}
-	if (line[i] == '\n')
-	{
-		s[i] = line[i];
-		i++;
-	}
-	s[i] = '\0';
-	return (s);
-}
-
-
-
-
-
-
-
-
-
-
-
-/*
 char *get_next_line(int fd)
 {
+    char *buffer;
+    char *line;
+    ssize_t nbytes;
+    static char *aux;
+
+    nbytes = 1;
+    line = ft_strdup("");
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (0);
+    buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+    if (!buffer)
+        return (0);
+    while (!strchr(line, '\n') && nbytes != 0)
+    {
+        nbytes = read(fd, buffer, BUFFER_SIZE);
+        if (nbytes < 0)
+        {
+            free (buffer);
+            return(0);
+        }
+        buffer[nbytes] = '\0';
+        line = strjoin(line, buffer);
+    }
+
+
+    int x = 0;
+    
+    while (line[x - 1] != '\n')
+    {
+        x++;
+    }
+    printf("%s\n", aux);
+    aux = ft_strdup(&line[x]);
 }
-*/
+
+int main(void)
+{
+    int fd;
+    char *str;
+
+    fd = open("filetbr.txt", O_RDONLY);
+    str = get_next_line(fd);
+    printf("%s", str);
+}
